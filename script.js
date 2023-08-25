39cdebf4d10c34deaeadeda46f8568b08d9ec8d3
Sergebouchrouch <143217803+Sergebouchrouch@users.noreply.github.com>
2023-08-25 22:17:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-  const button = document.querySelector("#myButton");
-  const heading = document.querySelector("h1");
+<!-- ...your existing HTML content... -->
 
-  button.addEventListener("click", function () {
-    heading.style.color = "blue";
-  });
-});
+<section class="rsvp">
+    <h2>RSVP</h2>
+    <p>Will you be joining us to celebrate our special day?</p>
+    <button id="rsvpButton">RSVP Now</button>
+    <p id="responseMessage"></p>
+</section>
 
+<!-- ...your existing HTML content... -->
+
+<script>
+    // Get references to the button and response message elements
+    const rsvpButton = document.getElementById("rsvpButton");
+    const responseMessage = document.getElementById("responseMessage");
+
+    // Add a click event listener to the button
+    rsvpButton.addEventListener("click", function () {
+        // Change the response message text
+        responseMessage.textContent = "Thank you for RSVPing! We look forward to celebrating with you.";
+    });
+</script>
+</body>
+</html>
