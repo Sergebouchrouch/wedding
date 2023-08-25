2d3fabe60f44dd0e41b61db1bcc56ee06f4b305d
Sergebouchrouch <143217803+Sergebouchrouch@users.noreply.github.com>
2023-08-25 21:38:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("#myButton");
+  const heading = document.querySelector("h1");
+
+  button.addEventListener("click", function () {
+    heading.style.color = "blue";
+  });
+});
 
